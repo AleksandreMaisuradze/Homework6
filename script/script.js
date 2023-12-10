07e0a8d242cae290284250aff493151747614cff
Aleksandre Maisuradze <aleksandremaisuradze11@gmail.com>
2023-12-10 23:21:08 +0400

    Finish todo list

---
+++ script/script.js
@@ -0,0 +1,31 @@
+let form = document.querySelector(".todo_form");
+let list = document.querySelector(".todo_list");
+let input = document.querySelector(".todo_form_input")
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+function add() {
+    if (input.value == "") {
+        alert("write something!")
+    } else {
+        let p = document.createElement("p")
+        let btn = document.createElement("button")
+        let listDiv = document.createElement("div")
+        list.appendChild(listDiv)
+        listDiv.classList.add("todo_list_div")
+        btn.classList.add("todo_list_div_button")
+        btn.textContent = "DELETE";
+        p.textContent = input.value;
+        listDiv.appendChild(p);
+        listDiv.appendChild(btn)
+        p.classList.add("todo_item")
+        input.value = "";
+        p.addEventListener("click", () => {
+            p.classList.toggle("checked")
+        })
+        btn.addEventListener("click", () => {
+            listDiv.remove();
+        })
+    }
+}
+
